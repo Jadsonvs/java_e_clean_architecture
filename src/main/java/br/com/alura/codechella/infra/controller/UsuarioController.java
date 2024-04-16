5f7dc75545857784652fac57e9463c44fe7346bf
Jadson Viana <jadson.vsantos2013@gmail.com>
2024-04-15 22:35:56 -0300

    Criando método controller para listagem de usuários

--- src/main/java/br/com/alura/codechella/infra/controller/UsuarioController.java
+++ src/main/java/br/com/alura/codechella/infra/controller/UsuarioController.java
@@ -1,11 +1,12 @@
 package br.com.alura.codechella.infra.controller;
 
 import br.com.alura.codechella.application.usecases.CadastrarUsuarioUseCase;
+import br.com.alura.codechella.application.usecases.ListarUsuarios;
 import br.com.alura.codechella.domain.entities.usuario.Usuario;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -13,15 +14,25 @@ public class UsuarioController {
 
     private final CadastrarUsuarioUseCase cadastrarUsuario;
 
+    private final ListarUsuarios listarUsuarios;
 
-    public UsuarioController(CadastrarUsuarioUseCase cadastrarUsuario) {
+
+    public UsuarioController(CadastrarUsuarioUseCase cadastrarUsuario, ListarUsuarios listarUsuarios) {
         this.cadastrarUsuario = cadastrarUsuario;
+        this.listarUsuarios = listarUsuarios;
     }
 
     @PostMapping
     public UsuarioDto cadastrarUsuario(@RequestBody UsuarioDto dto) {
         Usuario salvo = cadastrarUsuario.cadastrarUsuario(new Usuario(dto.cpf(), dto.nome(), dto.email(), dto.nascimento()));
         return new UsuarioDto(salvo.getCpf(), salvo.getNome(), salvo.getNascimento(), salvo.getEmail());
+    }
+
+    @GetMapping
+    public List<UsuarioDto> listarUsuario() {
+        return listarUsuarios.listarUsuarios().stream()
+                .map(u -> new UsuarioDto(u.getCpf(), u.getNome(), u.getNascimento(), u.getEmail()))
+                .collect(Collectors.toList());
     }
 
 }
